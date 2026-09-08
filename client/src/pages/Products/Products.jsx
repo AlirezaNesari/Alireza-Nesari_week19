f3ba0595b2c8063376ae839142e2a58a6c2d3244
AlirezaNesari <alirezanesari@gmail.com>
2026-09-09 00:00:15 +0330

    add JWT authorization interceptor

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,4 +1,21 @@
+import { useEffect } from "react";
+import api from "../../services/api";
+
 function Products() {
+  useEffect(() => {
+    const testApi = async () => {
+      try {
+        const response = await api.get("/products");
+
+        console.log("Products API:", response.data);
+      } catch (error) {
+        console.error("API Error:", error);
+      }
+    };
+
+    testApi();
+  }, []);
+
   return <h2>Products Page</h2>;
 }
 
