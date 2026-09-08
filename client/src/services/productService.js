9c99d22ec189a2eae140cd86c7513edb3e62d9dd
AlirezaNesari <alirezanesari@gmail.com>
2026-09-08 23:32:41 +0330

    setup API services for authentication and products

---
+++ client/src/services/productService.js
@@ -0,0 +1,37 @@
+import api from "./api";
+
+export const getProducts = async (params = {}) => {
+  const response = await api.get("/products", {
+    params,
+  });
+
+  return response.data;
+};
+
+export const getProductById = async (id) => {
+  const response = await api.get(`/products/${id}`);
+
+  return response.data;
+};
+
+export const createProduct = async (product) => {
+  const response = await api.post("/products", product);
+
+  return response.data;
+};
+
+export const updateProduct = async (id, product) => {
+  const response = await api.put(`/products/${id}`, product);
+
+  return response.data;
+};
+
+export const deleteProduct = async (id) => {
+  await api.delete(`/products/${id}`);
+};
+
+export const deleteProducts = async (ids) => {
+  await api.delete("/products", {
+    data: { ids },
+  });
+};
