fb1b9669d81e7990a654a31ad8091761e3138e76
AlirezaNesari <alirezanesari@gmail.com>
2026-09-12 20:06:41 +0330

    refactor: clean up unused product styles and imports

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,5 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
-
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
 import ProductModal from "../../components/ProductModal/ProductModal";
